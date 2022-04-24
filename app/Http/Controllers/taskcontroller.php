e78a87541c1aa40c42e9cd240e4c6721d2d1f7ae
Mohanned-Alshaer <mohannedelshaer@gmail.com>
2022-04-24 20:22:44 +0300

    first commit

--- app/Http/Controllers/taskcontroller.php
+++ app/Http/Controllers/taskcontroller.php
@@ -11,20 +11,22 @@ use App\Models\Task;
 class taskcontroller extends Controller {
     //
     public function index(){
-        // $tasks = Task::all();
-        $tasks = DB::table("tasks")->orderBy('name', 'asc')->get();
+        $tasks = Task::all();
+        // $tasks->name->orderBy('name', 'asc');
+        // $tasks = DB::table("tasks")->orderBy('name', 'asc')->get();
         return view("tasks.index",compact('tasks'));
     }
 
     public function show($id){
         // DB::table("tasks"
-        $task = DB::table("tasks")->find($id);
+        // $task = DB::table("tasks")->find($id);
+        $task = Task::find($id);
         return view("tasks.show",compact('task'));
     }
-
         // $task = DB::table("tasks")->insert([
         //     'name' => $request->name
         // ]);
+
     public function store(Request $request){
         $validated = $request->validate([
             'name' => "required |min:3|max:30"
@@ -34,6 +36,7 @@ class taskcontroller extends Controller {
         $task->save();
         return redirect()->back();
     }
+
     public function destroy($id){
         // DB::table('tasks')->where('id','=',$id)->delete();
         Task::find($id)->delete();
@@ -41,12 +44,15 @@ class taskcontroller extends Controller {
     }
 
     public function update($id){
-    //     DB::table('tasks')
-    //     ->where('id', $id)
-    //     ->update(['name' => "Mohanned"]);        
-    //    return redirect()->back();
-        $task = DB::table("tasks")->find($id);
-        $tasks = DB::table("tasks")->orderBy('name', 'asc')->get();
+        //     DB::table('tasks')
+        //     ->where('id', $id)
+        //     ->update(['name' => "Mohanned"]);        
+        //    return redirect()->back();
+
+        // $task = DB::table("tasks")->find($id);
+        $task = Task::find($id);
+        // $tasks = DB::table("tasks")->orderBy('name', 'asc')->get();
+        $tasks = Task::all();
         // $tasks = DB::table("tasks")->orderBy('name', 'asc')->get();
         return view("tasks.edit",compact('task','tasks'));
     }
@@ -54,12 +60,15 @@ class taskcontroller extends Controller {
         $validated = $request->validate([
             'name' => "required |min:3|max:30"
         ]);
-        $task = DB::table("tasks")->find($id);
+        $task = Task::find($id);
         $task->name = $request->input("name");
-        DB::table('tasks')
-            ->where('id', $id)
-            ->update(['name' => $task->name]);  
+        $task->save();
         return redirect("/");
+        // $task = DB::table("tasks")->find($id);
+        // DB::table('tasks')
+        //     ->where('id', $id)
+        //     ->update(['name' => $task->name]); 
+
         // $task->update();
         // $name = $request->input("name");
         // DB::update("update tasks set name=? WHERE id =$id",[$task->name]);
